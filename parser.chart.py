8fe75509f86509481cc773a1ada50f76e7d12dec
shellshock1953 <shellshock1953@gmail.com>
2017-01-31 00:28:30 +0000

    debug -- ok, netdata -- not

--- parser.chart.py
+++ parser.chart.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Description: CouchDB statistics netdata python.d module
 
-# from base import SimpleService
+from base import SimpleService
 
 import json
-import pprint
+
 try:
     import urllib.request as urllib2
 except ImportError:
@@ -13,16 +13,18 @@ except ImportError:
 # defaults
 priority = 70000
 retries = 60
-update_every = 1
+update_every = 10
+
+ORDER = []
+CHARTS = {}
 
 
 def parser(doc):
-    global ORDER
     result_dict = {}
     categories = doc.keys()
     ORDER = categories
     CHARTS = {}
-    lines = []
+    CHARTS_lines = []
 
     for category in categories:
         subcategories = doc[category].keys()
@@ -37,7 +39,7 @@ def parser(doc):
 
                 result_dict[category+'_'+subcategory+'_'+item] = \
                     doc[category][subcategory][item]
-                lines.append(
+                CHARTS_lines.append(
                     [item,
                      item,
                      'incremental', 1, 1 ]
@@ -45,20 +47,16 @@ def parser(doc):
 
         CHARTS[category] = {
             'options': [None, description, 'requests', '', '', 'stacked'],
-            'lines': lines
+            'lines': CHARTS_lines
         }
     return ORDER, CHARTS, result_dict
 
-#f = open('data.json','r').read()
-#doc = json.loads(f)
-#ORDER, CHARTS, result = parser(doc)
-#pprint.pprint(CHARTS)
-
 
 class Service(SimpleService):
-
     def __init__(self, configuration=None, name=None):
+
         SimpleService.__init__(self, configuration=configuration, name=name)
+
         self.couch_url = configuration['couch_url']
         if len(self.couch_url) == 0:
             raise Exception('Invalid couch_url')
@@ -66,13 +64,17 @@ class Service(SimpleService):
         try:
             response = urllib2.urlopen(self.couch_url).read()
             doc = json.loads(response)
-            ORDER, CHARTS, data = parser(doc)
+
+            ORDER, CHARTS, DATA = parser(doc)
+
         except (ValueError, AttributeError):
+            print('Error gotted!')
             return None
 
         self.order = ORDER
         self.definitions = CHARTS
-        self.data = data
+        self.data = DATA
 
-        def _get_data(self):
-            return self.data
+    def _get_data(self):
+        print('Inside _get_data')
+        return self.data
