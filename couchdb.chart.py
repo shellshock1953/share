6655ccf7f3b8c761a6f13f638a44826ce9f554fc
shellshock1953 <shellshock1953@gmail.com>
2017-02-01 19:09:28 +0200

    Nxx err

--- couchdb.chart.py
+++ couchdb.chart.py
@@ -14,7 +14,7 @@ priority = 70000
 retries = 60
 update_every = 1
 
-ORDER = ['httpd_queries','status_codes_queries']
+ORDER = ['httpd_queries', 'status_codes_queries']
 CHARTS = {
     'httpd_queries': {
         'options': [None, 'Http queries', 'requests', '',
@@ -32,23 +32,10 @@ CHARTS = {
         'options': [None, 'Status codes queries', 'requests', '',
                     '', 'stacked'],
         'lines': [
-            ['200_queries', '200', 'incremental', 1, 1],
-            ['201_queries', '201', 'incremental', 1, 1],
-            ['202_queries', '202', 'incremental', 1, 1],
-            ['301_queries', '301', 'incremental', 1, 1],
-            ['304_queries', '304', 'incremental', 1, 1],
-            ['400_queries', '400', 'incremental', 1, 1],
-            ['401_queries', '401', 'incremental', 1, 1],
-            ['403_queries', '403', 'incremental', 1, 1],
-            ['404_queries', '404', 'incremental', 1, 1],
-            ['405_queries', '405', 'incremental', 1, 1],
-            ['409_queries', '409', 'incremental', 1, 1],
-            ['412_queries', '412', 'incremental', 1, 1],
-            ['500_queries', '500', 'incremental', 1, 1],
-            ['409_queries', '409', 'incremental', 1, 1],
-            ['409_queries', '409', 'incremental', 1, 1],
-            ['409_queries', '409', 'incremental', 1, 1],
-            ['412_queries', '412', 'incremental', 1, 1]
+            ['2xx_queries', '2xx', 'incremental', 1, 1],
+            ['3xx_queries', '3xx', 'incremental', 1, 1],
+            ['4xx_queries', '4xx', 'incremental', 1, 1],
+            ['5xx_queries', '5xx', 'incremental', 1, 1]
         ]
     }
 }
@@ -70,25 +57,15 @@ class Service(SimpleService):
             'head_queries': 0,
             'post_queries': 0,
             'put_queries':  0,
-            '200_queries': 0,
-            '201_queries': 0,
-            '202_queries': 0,
-            '301_queries': 0,
-            '304_queries': 0,
-            '400_queries': 0,
-            '401_queries': 0,
-            '403_queries': 0,
-            '404_queries': 0,
-            '405_queries': 0,
-            '409_queries': 0,
-            '412_queries': 0,
-            '500_queries': 0
+            '2xx_queries': 0,
+            '3xx_queries': 0,
+            '4xx_queries': 0,
+            '5xx_queries': 0
         }
 
     def _get_data(self):
         try:
             response = urllib2.urlopen(self.couch_url).read()
-            
 
             httpd = json.loads(response)['httpd_request_methods']
             self.data['copy_queries'] = httpd['GET']['current']
@@ -99,22 +76,22 @@ class Service(SimpleService):
             self.data['put_queries'] = httpd['PUT']['current']
 
             status = json.loads(response)['httpd_status_codes']
-            self.data['200_queries'] = status['200']['current']
-            self.data['201_queries'] = status['201']['current']
-            self.data['202_queries'] = status['202']['current']
-            self.data['301_queries'] = status['301']['current']
-            self.data['304_queries'] = status['304']['current']
-            self.data['400_queries'] = status['400']['current']
-            self.data['401_queries'] = status['401']['current']
-            self.data['403_queries'] = status['403']['current']
-            self.data['404_queries'] = status['404']['current']
-            self.data['405_queries'] = status['405']['current']
-            self.data['409_queries'] = status['409']['current']
-            self.data['412_queries'] = status['412']['current']
-            self.data['500_queries'] = status['500']['current']
-            self.data['409_queries'] = status['409']['current']
-            self.data['409_queries'] = status['409']['current']
-            self.data['409_queries'] = status['409']['current']
+            self.data['2xx_queries'] =  status['200']['current'] + \
+                status['201']['current'] + \
+                status['202']['current']
+
+            self.data['3xx_queries'] =  status['301']['current'] + \
+                status['304']['current']
+
+            self.data['400_queries'] =  status['400']['current'] + \
+                status['401']['current'] + \
+                status['403']['current'] + \
+                status['404']['current'] + \
+                status['405']['current'] + \
+                status['409']['current'] + \
+                status['412']['current']
+
+            self.data['5xx_queries'] = status['500']['current']
 
             # replace CouchDB 'null' values with zero
             for key in self.data:
