c7e6a9694e84ec2b3760ff1e21aa6c181f56b547
shellshock1953 <shellshock1953@gmail.com>
2017-02-10 16:24:26 +0200

    added cache and opendocs

--- couchdb.chart.py
+++ couchdb.chart.py
@@ -14,7 +14,14 @@ priority = 70000
 retries = 60
 update_every = 1
 
-ORDER = ['httpd_queries', 'status_codes', 'io_statistics', 'database_fragmentation']
+ORDER = [
+    'httpd_queries',
+    'status_codes',
+    'io_statistics',
+    'database_fragmentation',
+    'auth_cache_hits',
+    'clients_requesting_changes',
+]
 CHARTS = {
     'httpd_queries': {
         'options': [None, 'Http queries', 'requests', '', '', 'stacked'],
@@ -52,11 +59,24 @@ CHARTS = {
             ['writes', 'db writes', 'absolute', 1, 1]
         ]
     },
+    'authenthentication_cache': {
+        'options': [None, 'Authentification cache', 'ration', '', '', 'stacked'],
+        'lines': [
+            ['cache_hits', 'cache hits', 'absolute', 1, 1],
+            ['cache_misses', 'cache misses', 'absolute', 1, 1]
+        ]
+    },
     'database_fragmentation': {
         'options': [None, 'Database fragmentation', 'Megabytes', '', '', 'stacked'],
         'lines': [
             ['disk_size_overhead', 'disk size overhead', 'absolute', 1, 1],
             ['data_size', 'data size', 'absolute', 1, 1]
+        ]
+    },
+    'clients_requesting_changes': {
+        'options': [None, 'CouchDB continuous changes listeners', 'clients', '', '', 'stacked'],
+        'lines': [
+            ['clients', 'clients for continuous changes', 'absolute', 1, 1]
         ]
     }
 }
@@ -73,12 +93,6 @@ class Service(SimpleService):
         self.order = ORDER
         self.definitions = CHARTS
         self.data = {
-            'COPY': 0,
-            'DELETE': 0,
-            'GET': 0,
-            'HEAD': 0,
-            'POST': 0,
-            'PUT': 0,
             '200': 0,
             '201': 0,
             '202': 0,
@@ -92,18 +106,31 @@ class Service(SimpleService):
             '409': 0,
             '412': 0,
             '500': 0,
+            'cache_hits': 0,
+            'cache_misses': 0,
+            'COPY': 0,
+            'DELETE': 0,
+            'GET': 0,
+            'HEAD': 0,
+            'POST': 0,
+            'PUT': 0,
+            'data_size': 0,
+            'disk_size_overhead': 0,
             'reads': 0,
             'writes': 0,
-            'data_size': 0,
-            'disk_size_overhead': 0
+            'clients': 0,
+            'docs': 0,
+            'docs_deleted': 0
         }
 
     def _get_data(self):
         try:
+            """ STATS """
             stats = urllib2.urlopen(self.couch_stats).read()
+            doc_stats = json.loads(stats)
 
             # http queries
-            httpd = json.loads(stats)['httpd_request_methods']
+            httpd = doc_stats['httpd_request_methods']
             self.data['COPY'] = httpd['COPY']['current']
             self.data['DELETE'] = httpd['DELETE']['current']
             self.data['GET'] = httpd['GET']['current']
@@ -112,7 +139,7 @@ class Service(SimpleService):
             self.data['PUT'] = httpd['PUT']['current']
 
             # http status codes
-            status = json.loads(stats)['httpd_status_codes']
+            status = doc_stats['httpd_status_codes']
             self.data['200'] = status['200']['current']
             self.data['201'] = status['201']['current']
             self.data['202'] = status['202']['current']
@@ -128,19 +155,30 @@ class Service(SimpleService):
             self.data['500'] = status['500']['current']
 
             # DB I/O
-            io = json.loads(stats)['couchdb']
-            self.data['db_reads'] = io['database_reads']['current']
-            self.data['db_writes'] = io['database_writes']['current']
+            couchdb = doc_stats['couchdb']
+            self.data['db_reads'] = couchdb['database_reads']['current']
+            self.data['db_writes'] = couchdb['database_writes']['current']
 
-            # fragmentation located in self.couch_db
-            # it is ('disk_size' - 'data_size')
-            database = urllib2.urlopen(self.couch_db).read()
-            frag = json.loads(database)
-            # convert to megabytes
-            # data_size and disk_size storaged in bytes
-            self.data['data_size'] = frag['data_size'] / 1000000
+            # Auth cache
+            self.data['cache_hits'] = couchdb['auth_cache_hits']['current']
+            self.data['cache_misses'] = couchdb['auth_cache_misses']['current']
+
+            # Clients requesting changes
+            self.data['clients'] = \
+                doc_stats['httpd']['clients_requesting_changes']['current']
+
+            """ EDGE_DB """
+            db = urllib2.urlopen(self.couch_db).read()
+            doc_db = json.loads(db)
+
+            # DB fragmentation
+            self.data['data_size'] = doc_db['data_size'] / 1000000
             self.data['disk_size_overhead'] = (
-                frag['disk_size'] - frag['data_size']) / 1000000
+                doc_db['disk_size'] - doc_db['data_size']) / 1000000
+
+            # DB documents
+            self.data['docs'] = doc_db['doc_count']
+            self.data['docs_deleted'] = doc_db['doc_del_count']
 
         except (ValueError, AttributeError):
             return self.data
