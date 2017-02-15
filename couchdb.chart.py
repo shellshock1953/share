f4f83d99765908092de35f2c52b798cc01cbc0b3
shellshock1953 <shellshock1953@gmail.com>
2017-02-15 18:46:43 +0200

    preready

--- couchdb.chart.py
+++ couchdb.chart.py
@@ -128,7 +128,6 @@ CHARTS = {
 # DELTA
 delta = {}
 
-
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
