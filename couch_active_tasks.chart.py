3d0dfe65ea17e70a8050256a57282f3f5f2be93a
shellshock1953 <shellshock1953@gmail.com>
2017-02-16 14:05:39 +0200

    must show all metric - even if None metrics present

--- couch_active_tasks.chart.py
+++ couch_active_tasks.chart.py
@@ -59,8 +59,8 @@ CHARTS = {
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
-        # self.couch_dbs = configuration['couch_dbs']
         # self.couch_tsk = configuration['couch_tsk']
+        # self.couch_dbs = configuration['couch_dbs']
         self.couch_tsk = 'http://127.0.0.1:5984/_active_tasks'
         self.couch_dbs = 'http://127.0.0.1:5984/_all_dbs'
         if len(self.couch_tsk) is 0:
@@ -77,59 +77,46 @@ class Service(SimpleService):
 
     def _get_data(self):
         try:
+            tasks_to_monitor = [ 'indexer', 'database_compaction', 'view_compaction', 'replication' ]
+
             # zero values EVERY time
             for key in self.data.keys():
                 self.data[key] = 0
 
-            # TODO get all dbs
-            # set dbs in .conf like a list
-            open_db = urllib2.urlopen(self.couch_dbs).read()
-            all_dbs = json.loads(open_db)
-            # DEBUG
-            # all_dbs = self.couch_dbs
+            #  open active tasks urls
+            active_tasks_url = urllib2.urlopen(self.couch_tsk).read()
+            active_tasks = json.loads(active_tasks_url)
+            #  open dbs urls
+            all_dbs_url = urllib2.urlopen(self.couch_dbs).read()
+            all_dbs = json.loads(all_dbs_url)
 
-            # TODO make dynamic types defined in .conf ???
-            tasks_to_monitor = [
-                'indexer',
-                'database_compaction',
-                'view_compaction',
-                'replication'
-            ]
+            # init task presentation
+            for monitoring_task in tasks_to_monitor:
+                self.data[monitoring_task + '_task'] = 0
 
-            # init db tasks
-            for available_db in all_dbs:
-                for available_task in tasks_to_monitor:
-                    self.data[available_task + '_' + available_db] = 0
+            # init DBs per task presentation
+            for monitoring_task in tasks_to_monitor:
+                for db in all_dbs:
+                    self.data[monitoring_task+'_'+db] = 0
+                    CHARTS[monitoring_task]['lines'].append([monitoring_task+'_'+db, db, 'absolute', 1, 1])
 
-            doc = urllib2.urlopen(self.couch_tsk).read()
-            # DEBUG
-            # doc = self.couch_tsk.read()
-            running_tasks = json.loads(doc)
+            # calculate running tasks
+            for active_task in active_tasks:
+                for monitoring_task in tasks_to_monitor:
+                    if monitoring_task == active_task['type']:
+                        self.data[monitoring_task + '_task'] += 1
 
-            # init 1st graph: count of all runnting tasks
-            for available_task in tasks_to_monitor:
-                for current_running_task in running_tasks:
-                    task_name = current_running_task['type']
-                    if task_name == available_task:
-                        self.data[task_name+'_task'] += 1
-
-            # init other graphs: DBs per task
-            for available_db in all_dbs:
-                for current_running_task in running_tasks:
-                    # try:
-                    #     db = current_running_task['database']
-                    # except KeyError:
-                    #     if '/' in current_running_task['target']:
-                    #         db_str = current_running_task['target']
-                    #         db = db_str.split('/')[3]
-                    #     else:
-                    #         db = current_running_task['target']
-
-                    task_name = current_running_task['type']
-                    chart_name = task_name + '_' + available_db
-                    CHARTS[task_name]['lines'].append([chart_name, available_db, 'absolute', 1, 1])
-                    self.data[task_name + '_' + available_db] += 1
-
+            # calculate dbs per task
+            for db in all_dbs:
+                for active_task in active_tasks:
+                    try: active_task_database = active_task['database']
+                    except KeyError:
+                        if '/' in active_task['target']:
+                            active_task_database_str = active_task['target']
+                            active_task_database = active_task_database_str.split('/')[3]
+                        else: active_task_database = active_task['target']
+                    if active_task_database == db:
+                        self.data[active_task['type']+'_'+db] += 1
 
         except (ValueError, AttributeError):
             return None
@@ -137,4 +124,3 @@ class Service(SimpleService):
 
 # s = Service(configuration={'priority': priority, 'retries': retries, 'update_every': update_every}, name=None)
 # d = s._get_data()
-# print CHARTS
