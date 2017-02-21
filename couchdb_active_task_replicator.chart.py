3bf05f8fd8db4746dc9e3f932eec11787fb31e45
shellshock1953 <shellshock1953@gmail.com>
2017-02-21 16:41:24 +0200

    check() added

--- couchdb_active_task_replicator.chart.py
+++ couchdb_active_task_replicator.chart.py
@@ -12,56 +12,55 @@ priority = 70000
 retries = 60
 update_every = 1
 
-ORDER = ['test']
-
-CHARTS = {
-    'test': {
-        'options': [None, 'TEST', 'tasks', 'Active tasks', '', 'line'],
-        'lines': [
-            ['test', 'test', 'absolute', 1, 1]
-        ]
-    }
-}
-
 
 class Service(SimpleService):
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration=configuration, name=name)
-        # self.active_tasks = open('/home/shellshock/share_DEBUG/active_task_repl.json').read()
-        self.active_tasks = open('/data/shellshock/share/active_task_repl.json').read()
-        self.order = ORDER
-        self.definitions = CHARTS
+        self.date = {}  # what to show
+        self.file = open('/data/shellshock/share/active_task_repl.json').read() #  where to get showing info
         self.data = {}
 
     def _get_data(self):
-        try:
-            tasks = json.loads(self.active_tasks)
-            for task in tasks:
-                source = task['source']
-                source_seq = task['source_seq']
-                my_seq = task['checkpointed_source_seq']
+        self.tasks = json.loads(self.file)
+        for task in self.tasks:
+            source = task['source']
+            source_seq = task['source_seq']
+            local_seq = task['source_seq']
 
-                CHARTS[source] = {
-                    'options': [None, 'Dyn source', 'tasks', '', '', 'line'],
-                    'lines': [
-                        ['source_seq', 'source_seq', 'absolute', 1, 1],
-                        ['my_seq', 'my_seq', 'absolute', 1, 1]
-                    ]
-                }
+            self.data[source + '_source_seq'] = source_seq
+            self.data[source + '_local_seq'] = local_seq
+
+    # if false -- exit
+    # used for dynamic chart creation
+    def check(self):
+        self.chart_creation()
+
+    def chart_creation(self):
+        for task in self.tasks:
+            source = task['source']
+
+            if source not in self.order:
+                self.order.append(source)
+
+                source_seq_var = source + '_source_seq'
+                local_seq_var = source + '_local_seq'
+
+                self.definitions.update({
+                        source: {
+                                'options': [None, 'Replications', 'seq', '', '', 'line'],
+                                'lines': [
+                                        [source_seq_var, 'source_seq', 'absolute', 1, 1],
+                                        [local_seq_var, 'local_seq', 'absolute', 1, 1],
+                                         ]
+                                }
+                        })
 
 
-                self.data['source_seq'] = source_seq
-                self.data['my_seq'] = my_seq
 
-
-                for chart in CHARTS:
-                    if chart not in ORDER:
-                        ORDER.append(chart)
-        except (ValueError, AttributeError):
-            return None
-        return self.data
 
 
 s = Service(configuration={'priority':60000,'retries':60,'update_every':1},name=None)
-d = s._get_data()
-print s.debug()
+s._get_data()
+s.check()
+print s.definitions
+print s.data
