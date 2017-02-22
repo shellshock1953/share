c52aebdf30d0dce5bf7faa18e7999d6b2c6d33b7
shellshock1953 <shellshock1953@gmail.com>
2017-02-22 20:17:24 +0200

    to broken to be used

--- couchdb_active_task_replicator.chart.py
+++ couchdb_active_task_replicator.chart.py
@@ -1,5 +1,5 @@
-# import sys
-# sys.path.append('/data/shellshock/install/netdata/python.d/python_modules')
+import sys
+sys.path.append('/data/shellshock/install/netdata/python.d/python_modules')
 
 from base import SimpleService
 import json
@@ -22,17 +22,21 @@ class Service(SimpleService):
         self.tasks = json.loads(self.file)
 
     def _get_data(self):
+        # zeroid data
         for key in self.data.keys():
             self.data[key] = 0
         try:
-            self.tasks = json.loads(self.file)
-            for task in self.tasks:
+            refresh_file = open('/data/shellshock/share/active_task_repl.json').read()
+            refresh_tasks = json.loads(refresh_file)
+            for task in refresh_tasks:
                 source = task['source']
                 source_seq = task['source_seq']
                 local_seq = task['source_seq']
 
                 self.data[source + '_source_seq'] = source_seq
                 self.data[source + '_local_seq'] = local_seq
+
+            # refresh_file.close()
             return self.data
         except:
             self.error("err in _get_data")
@@ -41,13 +45,16 @@ class Service(SimpleService):
     # used for dynamic chart creation
     def check(self):
         try:
-            self.chart_creation()
             return True
         except:
             self.error("err in check()")
             return False
 
     def chart_creation(self):
+        self.charts = ""
+        self.dimentions = ""
+        self._charts = ""
+        self._dimentions = ""
         try:
             for task in self.tasks:
                 source = task['source']
@@ -68,6 +75,7 @@ class Service(SimpleService):
             self.error("err in chart_creation()")
 
     def create(self):
+        self.chart_creation()
         try:
             for task in self.tasks:
                 source = task['source']
@@ -78,7 +86,9 @@ class Service(SimpleService):
             self.error("err in create()")
 
     def update(self, interval):
+        self.chart_creation()
         try:
+            SimpleService.create(self)
             for task in self.tasks:
                 source = task['source']
                 self.chart_name = source
@@ -89,9 +99,6 @@ class Service(SimpleService):
 
 # s = Service(configuration={'priority': 60000, 'retries': 60, 'update_every': 1}, name=None)
 # s._get_data()
-# import pdb; pdb.set_trace()
 # s.check()
 # s.create()
 # s.update(1)
-# print s.definitions
-# print s.data
