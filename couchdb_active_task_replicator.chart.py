37fad1d2cbab4a4fff9527da2000b5c2bab95d05
shellshock1953 <shellshock1953@gmail.com>
2017-02-23 19:55:36 +0200

    at last -- can create new chart even if new task appeared after check() was called TODO: integrate this into couchdb_active_task.chart to perform /_active_task global monitoring TODO: except task removal

--- couchdb_active_task_replicator.chart.py
+++ couchdb_active_task_replicator.chart.py
@@ -20,6 +20,7 @@ class Service(SimpleService):
         self.data = {}
         self.file = open('/data/shellshock/share/active_task_repl.json').read()
         self.tasks = json.loads(self.file)
+        self.new_charts = []
 
     def _get_data(self):
         # zeroid data
@@ -30,75 +31,100 @@ class Service(SimpleService):
             refresh_tasks = json.loads(refresh_file)
             for task in refresh_tasks:
                 source = task['source']
+
+                # Ops - got new source -- adding those for future chart_creation()
+                if source not in self.order:
+                    self.new_charts.append(source)
+
                 source_seq = task['source_seq']
                 local_seq = task['source_seq']
 
                 self.data[source + '_source_seq'] = source_seq
                 self.data[source + '_local_seq'] = local_seq
 
+
             # refresh_file.close()
             return self.data
         except:
             self.error("err in _get_data")
+            return None
 
     # if false -- exit
     # used for dynamic chart creation
+    # because check() runs before run()
     def check(self):
         try:
-            return True
+            status = self.chart_creation()
+            return status
         except:
             self.error("err in check()")
             return False
 
     def chart_creation(self):
-        self.charts = ""
-        self.dimentions = ""
-        self._charts = ""
-        self._dimentions = ""
-        try:
-            for task in self.tasks:
-                source = task['source']
-                if source not in self.order:
-                    self.order.append(source)
-                    source_seq_var = source + '_source_seq'
-                    local_seq_var = source + '_local_seq'
+        def create(source):
+            if source not in self.order:
+                # ORDER
+                self.order.append(source)
+                source_seq_var = source + '_source_seq'
+                local_seq_var = source + '_local_seq'
 
-                    self.definitions.update({
-                        source: {
-                            'options': [None, 'Replications', 'seq', '', '', 'line'],
-                            'lines': [
-                                [source_seq_var, 'source_seq', 'absolute', 1, 1],
-                                [local_seq_var, 'local_seq', 'absolute', 1, 1], ]
-                        }
-                    })
+                # CHARTS
+                self.definitions.update({
+                    source: {
+                        'options': [None, 'Replications', 'seq', '', '', 'line'],
+                        'lines': [
+                            [source_seq_var, 'source_seq', 'absolute', 1, 1],
+                            [local_seq_var, 'local_seq', 'absolute', 1, 1], ]
+                    }
+                })
+                self.create()
+
+        try:
+            for source in self.new_charts:
+                create(source)
+                self.new_charts.remove(source)
+            else:
+                for task in self.tasks:
+                    source = task['source']
+                    create(source)
+
+            return True
         except:
             self.error("err in chart_creation()")
+            return False
 
-    def create(self):
-        self.chart_creation()
-        try:
-            for task in self.tasks:
-                source = task['source']
-                self.chart_name = source
-                status = SimpleService.create(self)
-                return status
-        except:
-            self.error("err in create()")
 
+    # modified update() to check for a new replication tasks
     def update(self, interval):
-        self.chart_creation()
-        try:
-            SimpleService.create(self)
-            for task in self.tasks:
-                source = task['source']
-                self.chart_name = source
-                status = SimpleService.update(self, interval=interval)
-                return status
-        except:
-            self.error("err in update()")
+        data = self._get_data()
+        if data is None:
+            self.debug("failed to receive data during update().")
+            return False
 
-# s = Service(configuration={'priority': 60000, 'retries': 60, 'update_every': 1}, name=None)
-# s._get_data()
-# s.check()
-# s.create()
-# s.update(1)
+        updated = False
+        # do we have new charts?
+        if self.new_charts:
+            self.chart_creation()
+
+        for chart in self.order:
+            if self.begin(self.chart_name + "." + chart, interval):
+                updated = True
+                for dim in self.definitions[chart]['lines']:
+                    try:
+                        self.set(dim[0], data[dim[0]])
+                    except KeyError:
+                        pass
+                self.end()
+
+            # try to create new chart after plugin has been started
+
+        self.commit()
+        if not updated:
+            self.error("no charts to update")
+
+        return updated
+
+s = Service(configuration={'priority': 60000, 'retries': 60, 'update_every': 1}, name=None)
+s.check()
+s.update(1)
+s.run()
